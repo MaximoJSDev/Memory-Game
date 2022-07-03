4fe9df57fd4605e560b6dd0307e08574ffdd82b3
Maximo-4ever <maximilianocm06@gmail.com>
2022-07-02 19:36:34 -0500

    Añade aleatoriedad a la aparicion de las cartas

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-function Card() {
+function Card({ card }) {
   const [showCard, setShowCard] = useState(false);
   const changeVisibility = () => {
     setShowCard(true);
@@ -10,10 +10,10 @@ function Card() {
   };
 
   return (
-    <div className="card" onClick={changeVisibility}>
+    <div className="card" onClick={changeVisibility} idcard={card.id}>
       <img
         className={`card__img ${showCard && "showImg"}`}
-        src="https://images.cults3d.com/cjAUPjIz9fdOOZ0XRDHqzN3DM1k=/https://files.cults3d.com/uploaders/16051858/illustration-file/43d90dcb-d503-44a6-abf3-d383faca9f01/6.jpg"
+        src={card.img}
         alt="random"
       />
       <h3 className={`card__undiscovered ${showCard && "hideText"}`}>?</h3>
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,12 +1,43 @@
+import { useEffect, useState } from "react";
 import Card from "./Card";
 
 function Table() {
-  const cards = Array.from({ length: 16 });
+  const [numCards, setNumCards] = useState(Array.from({ length: 8 }));
+  const [cards, setCards] = useState([]);
+
+  const shuffleCards = () => {
+    for (let i = 0; i < numCards.length; i++) {
+      setCards((current) =>
+        [
+          ...current,
+          {
+            id: i,
+            img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+              i + 4
+            }.png`,
+            stat: "",
+          },
+          {
+            id: i,
+            img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+              i + 4
+            }.png`,
+            stat: "",
+          },
+        ].sort(() => Math.random() - 0.5)
+      );
+    }
+  };
+
+  useEffect(() => {
+    setCards([]);
+    shuffleCards();
+  }, []);
 
   return (
     <div className="table">
-      {cards.map((_, index) => (
-        <Card key={index} />
+      {cards.map((card, index) => (
+        <Card key={index} card={card} />
       ))}
     </div>
   );
