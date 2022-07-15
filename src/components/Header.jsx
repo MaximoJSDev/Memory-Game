a3d2b13fdb51d478636a79cd34bb74b975ffbfa7
Maximo-4ever <maximilianocm06@gmail.com>
2022-07-14 21:51:18 -0500

    Agrega el Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <header className="header">
+      <h1 className="title">Memory Game</h1>
+      <button className="btn btn-menu">Back to Menu &gt;&gt;</button>
+    </header>
+  );
+}
+
+export default Header;
